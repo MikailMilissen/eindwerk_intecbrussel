d82080958b8ad808021e1fa44398005a37c74bff
MikailMilissen <mikail.milissen@gmail.com>
2021-03-25 14:58:31 +0100

    'login'

--- eindwerk_intecbrussel/src/components/contexts/AuthContext.js
+++ eindwerk_intecbrussel/src/components/contexts/AuthContext.js
@@ -10,26 +10,32 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
 
     function register(email,password){
         return auth.createUserWithEmailAndPassword(email, password)
+    }
+    const login = (email,password) => {
+        return auth.signInWithEmailAndPassword(email, password)
     }
 
     useEffect(()=> {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
+            setLoading(false)
         })
         return unsubscribe
     }, [])
 
     const value = {
     currentUser,
-    register
+    register,
+    login
   }
 
     return(
         <AuthContext.Provider value={value}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     )
 }
