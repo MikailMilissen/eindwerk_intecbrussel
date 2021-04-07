5daf7ac32f5d31eab0fadc1c66f93a54120eaa07
susankuijpers <susankuijpers10@hotmail.com>
2021-04-07 17:15:14 +0200

    something

--- eindwerk_intecbrussel/src/components/Messageboard.js
+++ eindwerk_intecbrussel/src/components/Messageboard.js
@@ -16,21 +16,11 @@ const Messageboard = () => {
             <Link to='/messageboard/createmessage'>
             <img src="images/sla message icon .png" className="createmessage__button"/>
             </Link>
-            
-
 
             <div className="all_messages_container">
             {messages && <Message messages={messages} className='all_messages_div'/> }
             </div>
-<<<<<<< HEAD
-=======
             
-            <Link to='/messageboard/createmessage'>
-            <img src="images/sla icon.png" className="createmessage__button"/> 
-            <br />
-            <p className="create_message">CREATE MESSAGE</p>
-            </Link>
->>>>>>> 5d99a978a54856e9734931f04d6df5259b9c2adb
         </div>
 );
 }
