3ab744ce932a387da1f3e1a1af1ab4f4af39c603
susankuijpers <susankuijpers10@hotmail.com>
2021-04-08 17:29:03 +0200

    finished styling all the long components

--- eindwerk_intecbrussel/src/components/ArtistLong.js
+++ eindwerk_intecbrussel/src/components/ArtistLong.js
@@ -12,24 +12,24 @@ function ArtistLong() {
     const {data: artists, isPending, error} = useFetch('http://localhost:3000/artists/' + id) 
 
     return (
-        <div className='artists_details'>
+    <div className='artistlong_container'>
+        <div className='artist_details'>
             {isPending && <div>Loading..</div>}
             {error && <div>{error} </div>}
             {artists && (
         <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-            <CardTitle tag="h5">{artists.name}</CardTitle>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" className="cardimg" />
+        <CardBody>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+            <CardTitle tag="h2">{artists.name}</CardTitle>
             <CardText>{artists.bio}</CardText>
             
             {/* Maybe we can add social media icons here? */}
-            <Button>Get in Touch</Button>
+            <Button className="button">Get in Touch</Button>
         </CardBody>
         </Card>
-
         )}
-        
         </div>
+    </div>
     )
 }
 
