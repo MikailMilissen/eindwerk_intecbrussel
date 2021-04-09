74037d413a0b43cb33630d3f6253aaec487d7752
priyaved <priyaved87@gmail.com>
2021-04-09 13:58:06 +0200

    abt responsive

--- eindwerk_intecbrussel/src/components/About.js
+++ eindwerk_intecbrussel/src/components/About.js
@@ -12,13 +12,12 @@ function Homepage() {
       <br />
       <div className="about__text">
         <h1>ABOUT</h1>
-        <br />
         <p>SLA Platform is a social platform and online store<br />
         that helps to support local artists in Belgium.</p>
         <br />
-        <br />
+        
         <h1>CONTACT</h1>
-        <br />
+    
         <p>You can follow and contact us on:<br />
         Email: <a className="link" href="mailto: slaplatform@gmail.com">slaplatform@gmail.com</a><br />
         Tel.: 04 74 57 68 51<br />
